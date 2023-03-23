0f2acf86e770e79610d81c560bd8e56f0e948e50
Yvan Satyawan <y_satyawan@hotmail.com>
2023-03-23 16:10:51 +0100

    Implement signal processing base class

---
+++ src/signal_processing/base.py
@@ -0,0 +1,39 @@
+"""Base Signal Processing.
+
+Base abstract class for all signal processors.
+
+Author:
+    Yvan Satyawan <y_satyawan@hotmail.com>
+"""
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+
+class SignalProcessor(ABC):
+    """Base class for all signal processors."""
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def process(self, x: np.ndarray) -> np.ndarray:
+        """Processes the signal.
+
+        Args:
+            signal: Signal to process.
+
+        Returns:
+            Processed signal.
+        """
+        raise NotImplementedError
+
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        """Calls the process method.
+
+        Args:
+            signal: Signal to process.
+
+        Returns:
+            Processed signal.
+        """
+        return self.process(x)
