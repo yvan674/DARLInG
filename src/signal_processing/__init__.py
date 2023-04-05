b26729d9634c8d04005b2f85ab92a6d90c831799
Yvan Satyawan <y_satyawan@hotmail.com>
2023-04-05 15:11:27 +0200

    Added plot many signals

--- src/signal_processing/__init__.py
+++ src/signal_processing/__init__.py
@@ -3,6 +3,7 @@ from signal_processing.phase_unwrap import PhaseUnwrap
 
 import matplotlib.pyplot as plt
 
+
 def plot_signals(time_steps, original, processed, title, original_label,
                  processed_label):
     plt.title(title)
@@ -12,4 +13,28 @@ def plot_signals(time_steps, original, processed, title, original_label,
     plt.show()
 
 
-__all__ = ["LowPassFilter", "PhaseUnwrap", "plot_signals"]
+def plot_many_signals(time_steps, signals, labels, colors=None, title=None):
+    """Plot an arbitrary number of signals on the same figure.
+
+    Args:
+        time_steps (np.ndarray): The x-axis.
+        signals (list[np.ndarray]): The signals to plot. Should be 1D.
+        labels (list[str]): The labels for each signal.
+        colors (list[Any]): The color for each signal. Optional.
+        title (str): The title of the plot. Optional.
+    """
+    if title is not None:
+        plt.title(title)
+
+    if colors is not None:
+        for signal, label, color in zip(signals, labels, colors):
+            plt.plot(time_steps, signal, label=label, color=color)
+    else:
+        for signal, label in zip(signals, labels):
+            plt.plot(time_steps, signal, label=label)
+
+    plt.legend()
+    plt.show()
+
+
+__all__ = ["LowPassFilter", "PhaseUnwrap", "plot_signals", "plot_many_signals"]
