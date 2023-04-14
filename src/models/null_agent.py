8ba5788d7812b8ba302352c47a1c6df5f0fd7dfe
Yvan Satyawan <y_satyawan@hotmail.com>
2023-04-14 13:48:51 +0200

    Implement null_agent

---
+++ src/models/null_agent.py
@@ -0,0 +1,45 @@
+"""Null Agent.
+
+Only produces null embeddings.
+
+Authors:
+    Yvan Satyawan <y_satyawan@hotmail.com>
+"""
+import torch
+
+from models.base_embedding_agent import BaseEmbeddingAgent
+
+
+class NullAgent(BaseEmbeddingAgent):
+    def __init__(self, embedding_shape: tuple, null_value: float):
+        super().__init__()
+        self.device = None
+        self.size = embedding_shape
+        self.null_value = null_value
+
+    def _produce_action(self, observation: torch.Tensor,
+                        info: None | dict[any]) -> torch.Tensor:
+        return torch.full(self.size, fill_value=self.null_value,
+                          device=self.device)
+
+    def process_reward(self, observation: torch.Tensor, reward: float):
+        pass
+
+    def train(self):
+        pass
+
+    def eval(self):
+        pass
+
+    def state_dict(self):
+        return {"device": self.device,
+                "size": self.size,
+                "null_value": self.null_value}
+
+    def load_state_dict(self, sd: dict[any]):
+        self.device = sd["device"]
+        self.size = sd["size"]
+        self.null_value = sd["null_value"]
+
+    def to(self, device: int | torch.device | None):
+        self.device = device
