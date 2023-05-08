6d747f15659d27c592bc1bc421d8005466e871a7
Yvan Satyawan <y_satyawan@hotmail.com>
2023-05-08 17:22:34 +0200

    KnownDomainAgent updated to use the new collation of info

--- src/models/known_domain_agent.py
+++ src/models/known_domain_agent.py
@@ -19,13 +19,18 @@ class KnownDomainAgent(BaseEmbeddingAgent):
         return "KnownDomainAgent()"
 
     def _produce_action(self, observation: torch.Tensor,
-                        info: list[dict[str, any]]) -> torch.Tensor:
+                        info: dict[str, list[any]]) -> torch.Tensor:
         """Produce an action based on the info dictionary.
 
         The info dictionary looks like:
-            {'user': '2', 'room_num': 1, 'date': '20181109',
-             'torso_location': '1', 'face_orientation': '2',
-             'gesture': '4'}.
+            {
+                'user': [2,...],
+                'room_num': [1,...],
+                'date': ['20181109',...],
+                'torso_location': ['1',...],
+                'face_orientation': ['2',...],
+                'gesture': ['4',...]
+            }.
         We use everything except the date and gesture as part of our known
         domain embedding. The values are all transformed to a one-hot encoding.
         The one-hot encoding is hardcoded, as we know what the info dictionary
@@ -33,13 +38,18 @@ class KnownDomainAgent(BaseEmbeddingAgent):
         """
         # 17 users, 3 rooms, 8 torso locations, 5 face orientations
         # Total length of 33
-        embedding = torch.zeros((len(info), 33), device=self.device)
-        for i, sample_info in enumerate(info):
-            # -1 since the info dictionary is 1-indexed instead of 0-indexed.
-            user_val = int(sample_info["user"]) - 1
-            room_val = 17 + int(sample_info["room_num"]) - 1
-            torso_val = 17 + 3 + int(sample_info["torso_location"]) - 1
-            face_val = 17 + 3 + 8 + int(sample_info["face_orientation"]) - 1
+        batch_size = len(info["user"])
+        embedding = torch.zeros((batch_size, 33), device=self.device)
+        # Extract required keys
+        info = {k: info[k]
+                for k in ("user", "room_num", "torso_location",
+                          "face_orientation")}
+        # Not the usual one-hot way since this is kinda weirdly formed
+        for i in range(len(info)):
+            user_val = info["user"][i]
+            room_val = 17 + info["room_num"][i]
+            torso_val = 17 + 3 + info["torso_location"][i]
+            face_val = 17 + 3 + 8 + info["face_orientation"][i]
             embedding[i, [user_val, room_val, torso_val, face_val]] = 1.
 
         return embedding
