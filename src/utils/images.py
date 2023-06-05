43a054df72da649718b597ff4ca9bf916d7236b6
Yvan Satyawan <y_satyawan@hotmail.com>
2023-06-05 14:47:50 +0200

    Made tensor-to-image take the 14th image

--- src/utils/images.py
+++ src/utils/images.py
@@ -19,7 +19,9 @@ def tensor_to_image(data: torch.Tensor, img_idxs: tuple,
     Returns:
         An image in mode "rgb" with the given palette.
     """
-    img_arrays = data[img_idxs[0]:img_idxs[1], 0, :, :] \
+    # We take the 14th image, the "middle" image since there are always
+    # 28 images.
+    img_arrays = data[img_idxs[0]:img_idxs[1], 14, :, :] \
         .detach() \
         .cpu() \
         .numpy() \
