9cbfcdec95fba0256a868286fa70292a56f77a3c
Yvan Satyawan <y_satyawan@hotmail.com>
2023-07-05 16:50:03 +0700

    Fixed typo in type checking

--- src/data_utils/widar_dataset.py
+++ src/data_utils/widar_dataset.py
@@ -136,7 +136,7 @@ class WidarDataset(Dataset):
              CSI [pn, cn, an, 1] where pn is the packet number, cn the
              subcarrier channel number, and an the antenna number.
          """
-        if self.dataset_type == "is_small":
+        if self.dataset_type == "small":
             # widar_small moves the files to a different location, so we
             # overwrite it here.
             csi_file_path = self.data_path / csi_file_path.name
