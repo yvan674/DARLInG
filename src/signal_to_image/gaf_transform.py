4817be36ba500732c04c0af127211ee1a39612cd
Yvan Satyawan <y_satyawan@hotmail.com>
2023-09-03 16:42:11 +0200

    Changed default image_size for transforms

--- src/signal_to_image/gaf_transform.py
+++ src/signal_to_image/gaf_transform.py
@@ -10,7 +10,7 @@ from signal_to_image.base import SignalToImageTransformer
 
 class GAF(SignalToImageTransformer):
     def __init__(self,
-                 image_size: int | float = 0.05,
+                 image_size: int | float = 0.02,
                  normalize: bool = True,
                  method: str = "summation",
                  overlapping: bool = False,
