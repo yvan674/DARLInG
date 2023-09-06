a59144c043d461eb93810f67a2c0bc726ec5f4fb
Yvan Satyawan <y_satyawan@hotmail.com>
2023-09-06 15:25:16 +0200

    Updated latent_space_exploration.py to actual working

--- src/experiments/latent_space_exploration.py
+++ src/experiments/latent_space_exploration.py
@@ -1,7 +1,10 @@
 """Latent Space Exploration."""
+import copy
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
 
+import numpy as np
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 import torch
 import matplotlib.pyplot as plt
@@ -9,6 +12,8 @@ from sklearn.manifold import TSNE
 
 from data_utils.widar_dataset import WidarDataset
 from models.model_builder import build_model
+from rl.rl_builder import build_rl
+from rl.reward_functions import func_from_str
 from utils.config_parser import parse_config_file
 
 
@@ -16,6 +21,8 @@ def parse_args() -> Namespace:
     p = ArgumentParser()
     p.add_argument("checkpoint_fp", type=Path,
                    help="Path to the checkpoint")
+    p.add_argument("agent_checkpoint_fp", type=Path,
+                   help="Path to the agent checkpoint")
     p.add_argument("config_fp", type=Path,
                    help="Path to the config file")
     p.add_argument("data_dir_fp", type=Path,
@@ -36,7 +43,7 @@ def forward_pass(encoder, embed_agent, amp, phase, bvp, bvp_pipeline, device):
 
     with torch.no_grad():
         z = encoder(amp, phase, bvp)[0]
-        action = embed_agent(z)[0]
+        action = embed_agent.predict(z.detach().cpu())[0]
 
     return z, action
 
@@ -44,6 +51,8 @@ def forward_pass(encoder, embed_agent, amp, phase, bvp, bvp_pipeline, device):
 def visualize_space(space, colors, legend_ncols, space_name, plot_title,
                     legend_title):
     fig, ax = plt.subplots()
+    # set dpi to 300 for high quality
+    fig.dpi = 300
     scatter = ax.scatter(space[:, 0], space[:, 1],
                          c=colors)
 
@@ -54,15 +63,22 @@ def visualize_space(space, colors, legend_ncols, space_name, plot_title,
 
     # Put legend below the axis
     ax.legend(*scatter.legend_elements(),
-              loc='upper center', bbox_to_anchor=(0.5, -0.85),
+              loc="upper center", bbox_to_anchor=(0.5, -0.05),
               ncol=legend_ncols, title=legend_title)
     plt.title(f"{space_name} ({plot_title})")
-    plt.savefig(f"{space_name}_{plot_title}.png")
+    plt.savefig(f"../../figures/{space_name}_{plot_title}.png")
 
 
-def main(checkpoint_fp: Path, config_fp: Path, data_dir_fp: Path):
+def main(checkpoint_fp: Path, agent_checkpoint_fp: Path,
+         config_fp: Path, data_dir_fp: Path):
     config = parse_config_file(config_fp)
-    device = torch.device("cpu")
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+    # device = torch.device("cpu")
 
     # SECTION Data
     bvp_pipeline = config["data"]["bvp_pipeline"]
@@ -83,77 +99,73 @@ def main(checkpoint_fp: Path, config_fp: Path, data_dir_fp: Path):
                                  phase_pipeline=config["data"][
                                      "phase_pipeline"])
 
+    train_loader = DataLoader(train_dataset)
+
     # SECTION Model
-    encoder, _, _, embed_agent = build_model(config, train_dataset)
+    reward_function = func_from_str(config["embed"]["reward_function"])
+    encoder, null_head, null_agent = build_model(config, train_dataset)
+    embed_head = copy.deepcopy(null_head)
+
+    env, embed_agent, _ = build_rl(encoder, null_head, embed_head, null_agent,
+                                   config["embed"]["agent_type"],
+                                   bvp_pipeline, device, train_loader,
+                                   reward_function, 1)
 
     encoder.to(device)
-    embed_agent.to(device)
 
     # Load checkpoint
     checkpoint = torch.load(checkpoint_fp, map_location=device)
     encoder.load_state_dict(checkpoint["encoder_state_dict"])
 
-    # Temporary Workaround
-    embed_agent_state_dict = checkpoint["embed_agent_state_dict"]
-    if "critic_num_layers" not in embed_agent_state_dict:
-        embed_agent_state_dict.update({"critic_num_layers": 4,
-                                       "critic_dropout": 0.562851983704799,
-                                       "actor_num_layers": 6,
-                                       "actor_dropout": 0.47659867398346056})
-    embed_agent.load_state_dict(checkpoint["embed_agent_state_dict"])
+    embed_agent.load(agent_checkpoint_fp)
 
     encoder.eval()
-    embed_agent.eval()
 
     # SECTION Latent space exploration
     latent_space = []
     embed_space = []
     infos = []
-    users = set()
-    for i, (amp, phase, bvp, info) in tqdm(enumerate(train_dataset),
-                                           total=len(train_dataset)):
-        z, action = forward_pass(encoder, embed_agent, amp, phase, bvp,
-                                 bvp_pipeline, device)
-        latent_space.append(z)
-        embed_space.append(action)
-        info["is_train"] = True
-        infos.append(info)
-        users.add(info["user"])
 
-    for i, (amp, phase, bvp, info) in tqdm(enumerate(valid_dataset),
-                                           total=len(valid_dataset)):
-        z, action = forward_pass(encoder, embed_agent, amp, phase, bvp,
-                                 bvp_pipeline, device)
-        latent_space.append(z)
-        embed_space.append(action)
-        info["is_train"] = False
-        infos.append(info)
-        users.add(info["user"])
+    for dataset, is_train in zip((train_dataset, valid_dataset),
+                                 (True, False)):
+        for i, (amp, phase, bvp, info) in tqdm(enumerate(dataset),
+                                               total=len(dataset)):
+            z, action = forward_pass(encoder, embed_agent, amp, phase, bvp,
+                                     bvp_pipeline, device)
+            latent_space.append(z)
+            embed_space.append(action)
+            info["is_train"] = is_train
+            info["user"] += 1
+            infos.append(info)
 
-    latent_space = torch.cat(latent_space, dim=0)
-    embed_space = torch.cat(embed_space, dim=0)
+    latent_space = torch.cat(latent_space, dim=0).detach().cpu().numpy()
+    embed_space = np.concatenate(embed_space, axis=0)
 
 
     # SECTION Visualize spaces
-    for space, space_name in zip([latent_space, embed_space],
-                                 ["latent_space", "embed_space"]):
+    for space, space_name in tqdm(zip([latent_space, embed_space],
+                                      ["Latent Space t-SNE",
+                                       "Domain Embedding t-SNE"]),
+                                  total=2):
         tsne = TSNE(n_components=2, random_state=0)
         space = tsne.fit_transform(space)
 
         # Color based on train/valid, showing legend
         visualize_space(space, [info["is_train"] for info in infos],
-                        2, space_name, "train-valid",
+                        2, space_name,
+                        "Train vs. Validation set",
                         "Is Train")
 
         visualize_space(space, [info["gesture"] for info in infos],
-                        6, space_name, "gesture",
+                        6, space_name, "Gesture",
                         "Gesture")
 
         visualize_space(space, [info["user"] for info in infos],
-                        6, space_name, "user",
+                        6, space_name, "User",
                         "User")
 
 
 if __name__ == '__main__':
     args = parse_args()
-    main(args.checkpoint_fp, args.config_fp, args.data_dir_fp)
+    main(args.checkpoint_fp, args.agent_checkpoint_fp,
+         args.config_fp, args.data_dir_fp)
