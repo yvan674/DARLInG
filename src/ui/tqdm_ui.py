b5f7682b32ecbef1a0bb397479377bf1c497da32
Yvan Satyawan <y_satyawan@hotmail.com>
2023-09-09 12:12:13 +0200

    Refactor a lot of init to base_ui.py

--- src/ui/tqdm_ui.py
+++ src/ui/tqdm_ui.py
@@ -13,23 +13,21 @@ class TqdmUI(BaseUI):
     def __init__(self, train_steps: int, valid_steps: int, epochs: int,
                  initial_data: dict[str, any]):
         super().__init__(train_steps, valid_steps, epochs, initial_data)
-        total_steps = (valid_steps + train_steps) * epochs
-        self.current_epoch = 1
-        self.total_epochs = epochs
         self.prog_bar = tqdm(desc=f"Epoch: 1 of {epochs} | "
-                                  f"Training loss: -.--", total=total_steps)
+                                  f"Training loss: -.--",
+                             total=self.total_steps)
 
     def update_data(self, data: dict[str, any]):
         if "epoch" in data:
             self.current_epoch = data["epoch"] + 1
         if "train_loss" in data:
             self.prog_bar.set_description(f"Epoch: {self.current_epoch} of "
-                                          f"{self.total_epochs} | "
+                                          f"{self.epochs} | "
                                           f"Training loss: "
                                           f"{data['train_loss']:.2f}")
         elif "valid_loss" in data:
             self.prog_bar.set_description(f"Epoch: {self.current_epoch} of "
-                                          f"{self.total_epochs} | "
+                                          f"{self.epochs} | "
                                           f"Validation loss: "
                                           f"{data['valid_loss']:.2f}")
         for key in data.keys():
